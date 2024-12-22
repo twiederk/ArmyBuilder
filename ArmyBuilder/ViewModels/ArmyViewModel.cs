db07b973e5036fdf7767a4adb99232e3e769e21c
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-12-22 06:29:48 +0100

    Display each army category in separate tab

--- ArmyBuilder/ViewModels/ArmyViewModel.cs
+++ ArmyBuilder/ViewModels/ArmyViewModel.cs
@@ -10,7 +10,12 @@ namespace ArmyBuilder.ViewModels
         private readonly IArmyBuilderRepository _repository;
         private ArmyList _selectedArmyList;
         private List<MainModel> _mainModels;
+        private List<MainModel> _characters;
+        private List<MainModel> _troopers;
+        private List<MainModel> _warMachines;
+        private List<MainModel> _monsters;
         private MainModel _selectedMainModel;
+
 
         public ArmyViewModel(IArmyBuilderRepository repository)
         {
@@ -38,6 +43,45 @@ namespace ArmyBuilder.ViewModels
             }
         }
 
+        public List<MainModel> Characters
+        {
+            get => _characters;
+            private set
+            {
+                _characters = value;
+                OnPropertyChanged(nameof(Characters));
+            }
+        }
+
+        public List<MainModel> Troopers
+        {
+            get => _troopers;
+            private set
+            {
+                _troopers = value;
+                OnPropertyChanged(nameof(Troopers));
+            }
+        }
+
+        public List<MainModel> WarMachines
+        {
+            get => _warMachines;
+            private set
+            {
+                _warMachines = value;
+                OnPropertyChanged(nameof(WarMachines));
+            }
+        }
+        public List<MainModel> Monsters
+        {
+            get => _monsters;
+            private set
+            {
+                _monsters = value;
+                OnPropertyChanged(nameof(Monsters));
+            }
+        }
+
         public MainModel SelectedMainModel
         {
             get => _selectedMainModel;
@@ -53,10 +97,18 @@ namespace ArmyBuilder.ViewModels
             if (_selectedArmyList != null)
             {
                 MainModels = _repository.MainModels(_selectedArmyList.Id);
+                Characters = MainModels.FindAll(mm => mm.ArmyCategory == ArmyCategory.Character);
+                Troopers = MainModels.FindAll(mm => mm.ArmyCategory == ArmyCategory.Trooper);
+                WarMachines = MainModels.FindAll(mm => mm.ArmyCategory == ArmyCategory.WarMachine);
+                Monsters = MainModels.FindAll(mm => mm.ArmyCategory == ArmyCategory.Monster);
             }
             else
             {
                 MainModels = new List<MainModel>();
+                Characters = new List<MainModel>();
+                Troopers = new List<MainModel>();
+                WarMachines = new List<MainModel>();
+                Monsters = new List<MainModel>();
             }
         }
 
