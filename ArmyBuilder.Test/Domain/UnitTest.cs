55dd0f79c56064ee294fb39a66c0dd193ac1c13b
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-12-23 10:24:48 +0100

    Added class Unit

---
+++ ArmyBuilder.Test/Domain/UnitTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using ArmyBuilder.Domain;
+
+namespace ArmyBuilder.Test.Domain
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void should_create_unit_with_name()
+        {
+            // act
+            var unit = new Unit("Test Unit");
+
+            // assert
+            unit.Name.Should().Be("Test Unit");
+
+        }
+
+        [Fact]
+        public void should_calculate_points_of_unit()
+        {
+            // arrange
+            var unit = new Unit("Test Unit");
+
+            // act
+            var points = unit.Points();
+
+            // assert
+            points.Should().Be(75);
+        }
+    }
+}
