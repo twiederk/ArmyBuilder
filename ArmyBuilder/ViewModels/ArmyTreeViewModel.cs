9682d8cacd00cdc9623e18cd278500f2e40a1ef6
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-12-23 10:12:47 +0100

    Use MainModel as data for MainModelNode

--- ArmyBuilder/ViewModels/ArmyTreeViewModel.cs
+++ ArmyBuilder/ViewModels/ArmyTreeViewModel.cs
@@ -23,13 +23,36 @@ namespace ArmyBuilder.ViewModels
                             Value = 0,
                             Children = new ObservableCollection<MainModelTreeNode>
                             {
-                                new MainModelTreeNode { 
-                                    Name = "General",
-                                    Value = 100,
+                                new MainModelTreeNode(
+                                    new MainModel()
+                                    {
+                                        Name = "General",
+                                        Points = 100,
+                                        SingleModels = new List<SingleModel>
+                                        {
+                                            new SingleModel()
+                                            {
+                                                Name = "General",
+                                                Profile = new Profile
+                                                {
+                                                    Movement = 5,
+                                                    WeaponSkill = 7,
+                                                    BallisticSkill = 7,
+                                                    Strength = 4,
+                                                    Toughness = 4,
+                                                    Wounds = 3,
+                                                    Initiative = 9,
+                                                    Attacks = 4,
+                                                    Moral = 10
+                                                }
+                                            }
+                                        }
+                                    }
+                                    ) {
                                     Count = 1,
                                     Children = new ObservableCollection<SingleModelTreeNode>
                                     {
-                                        new SingleModelTreeNode( 
+                                        new SingleModelTreeNode(
                                             new SingleModel() {
                                                 Name = "General",
                                                 Profile = new Profile {
@@ -47,9 +70,32 @@ namespace ArmyBuilder.ViewModels
                                         ),
                                     }
                                 },
-                                new MainModelTreeNode {
-                                    Name = "Speertr‰ger",
-                                    Value = 10,
+                                new MainModelTreeNode(
+                                    new MainModel()
+                                    {
+                                        Name = "Speertr‰ger",
+                                        Points = 10,
+                                        SingleModels = new List<SingleModel>
+                                        {
+                                            new SingleModel()
+                                            {
+                                                Name = "Speertr‰ger",
+                                                Profile = new Profile
+                                                {
+                                                    Movement = 5,
+                                                    WeaponSkill = 4,
+                                                    BallisticSkill = 4,
+                                                    Strength = 3,
+                                                    Toughness = 3,
+                                                    Wounds = 1,
+                                                    Initiative = 6,
+                                                    Attacks = 1,
+                                                    Moral = 8
+                                                }
+                                            }
+                                        }
+                                    }
+                                    ) {
                                     Count = 20,
                                     Children = new ObservableCollection<SingleModelTreeNode>
                                     {
@@ -79,9 +125,64 @@ namespace ArmyBuilder.ViewModels
                             Value = 70,
                             Children = new ObservableCollection<MainModelTreeNode>
                             {
-                                new MainModelTreeNode {
-                                    Name = "Streitwagen",
-                                    Value = 70,
+                                new MainModelTreeNode(
+                                    new MainModel()
+                                    {
+                                        Name = "Streitwagen",
+                                        Points = 70,
+                                        SingleModels = new List<SingleModel>
+                                        {
+                                            new SingleModel()
+                                            {
+                                                Name = "Streitwagen",
+                                                Profile = new Profile
+                                                {
+                                                    Movement = 0,
+                                                    WeaponSkill = 0,
+                                                    BallisticSkill = 0,
+                                                    Strength = 0,
+                                                    Toughness = 7,
+                                                    Wounds = 3,
+                                                    Initiative = 0,
+                                                    Attacks = 0,
+                                                    Moral = 0
+                                                }
+                                            },
+                                            new SingleModel()
+                                            {
+                                                Name = "Streitwagenlenker",
+                                                Profile = new Profile
+                                                {
+                                                    Movement = 5,
+                                                    WeaponSkill = 5,
+                                                    BallisticSkill = 4,
+                                                    Strength = 4,
+                                                    Toughness = 3,
+                                                    Wounds = 1,
+                                                    Initiative = 7,
+                                                    Attacks = 1,
+                                                    Moral = 8
+                                                }
+                                            },
+                                            new SingleModel()
+                                            {
+                                                Name = "Elfenroﬂ",
+                                                Profile = new Profile
+                                                {
+                                                    Movement = 9,
+                                                    WeaponSkill = 3,
+                                                    BallisticSkill = 0,
+                                                    Strength = 3,
+                                                    Toughness = 3,
+                                                    Wounds = 1,
+                                                    Initiative = 4,
+                                                    Attacks = 1,
+                                                    Moral = 5
+                                                }
+                                            }
+                                        }
+                                        }
+                                    ) {
                                     Count = 1,
                                     Children = new ObservableCollection<SingleModelTreeNode>
                                     {
--- ArmyBuilder/ViewModels/MainModelTreeNode.cs
+++ ArmyBuilder/ViewModels/MainModelTreeNode.cs
@@ -1,13 +1,22 @@
 using System.Collections.ObjectModel;
+using ArmyBuilder.Domain;
 
 namespace ArmyBuilder.ViewModels
 {
     public class MainModelTreeNode
     {
-        public string Name { get; set; }
-        public int Value { get; set; }
+        public string Name => _mainModel.Name;
+        public float Points => _mainModel.Points;
         public int Count { get; set; }
+
+        private MainModel _mainModel;
+
         public ObservableCollection<SingleModelTreeNode> Children { get; set; } = new ObservableCollection<SingleModelTreeNode>();
+
+        public MainModelTreeNode(MainModel mainModel)
+        {
+            _mainModel = mainModel;
+        }
     }
 }
 
