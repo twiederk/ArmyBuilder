37c51caa8640bdcb35224f0c428861cd602be9ad
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-12-23 16:20:23 +0100

    Use Army.Units to create UnitTreeNodes

--- ArmyBuilder/ViewModels/ArmyTreeNode.cs
+++ ArmyBuilder/ViewModels/ArmyTreeNode.cs
@@ -7,14 +7,23 @@ namespace ArmyBuilder.ViewModels
     {
         public string Name => _army.Name;
         public int Points => _army.Points();
+        public ObservableCollection<UnitTreeNode> Children { get; set; } = new ObservableCollection<UnitTreeNode>();
+
         private Army _army;
 
         public ArmyTreeNode(Army army)
         {
             _army = army;
+            SetChildren();
         }
 
+        private void SetChildren()
+        {
+            foreach (var unit in _army.Units)
+            {
+                Children.Add(new UnitTreeNode(unit));
+            }
+        }
 
-        public ObservableCollection<UnitTreeNode> Children { get; set; } = new ObservableCollection<UnitTreeNode>();
     }
 }
--- ArmyBuilder/ViewModels/ArmyTreeViewModel.cs
+++ ArmyBuilder/ViewModels/ArmyTreeViewModel.cs
@@ -11,136 +11,129 @@ namespace ArmyBuilder.ViewModels
         public ArmyTreeViewModel()
         {
             Army army = new Army("Armee der Hochelfen von Tyr");
+            army.Units = new List<Unit>()
+            {
+                new Unit("Generalseinheit")
+                {
+                    MainModels = new List<Pair<int, MainModel>>
+                    {
+                        new Pair<int, MainModel>(1, new MainModel()
+                        {
+                            Name = "General",
+                            Points = 100,
+                            SingleModels = new List<SingleModel>
+                            {
+                                new SingleModel()
+                                {
+                                    Name = "General",
+                                    Profile = new Profile
+                                    {
+                                        Movement = 5,
+                                        WeaponSkill = 7,
+                                        BallisticSkill = 7,
+                                        Strength = 4,
+                                        Toughness = 4,
+                                        Wounds = 3,
+                                        Initiative = 9,
+                                        Attacks = 4,
+                                        Moral = 10
+                                    }
+                                }
+                            }
+                        }),
+                        new Pair<int, MainModel>(20, new MainModel()
+                        {
+                            Name = "Speertr‰ger",
+                            Points = 10,
+                            SingleModels = new List<SingleModel>
+                            {
+                                new SingleModel()
+                                {
+                                    Name = "Speertr‰ger",
+                                    Profile = new Profile
+                                    {
+                                        Movement = 5,
+                                        WeaponSkill = 4,
+                                        BallisticSkill = 4,
+                                        Strength = 3,
+                                        Toughness = 3,
+                                        Wounds = 1,
+                                        Initiative = 6,
+                                        Attacks = 1,
+                                        Moral = 8
+                                    }
+                                }
+                            }
+                        })
+                    }
+
+                },
+                new Unit("Streitwagen")
+                {
+                    MainModels = new List<Pair<int, MainModel>>
+                    {
+                        new Pair<int, MainModel>(1, new MainModel()
+                        {
+                            Name = "Streitwagen",
+                            Points = 70,
+                            SingleModels = new List<SingleModel>
+                            {
+                                new SingleModel()
+                                {
+                                    Name = "Streitwagen",
+                                    Profile = new Profile
+                                    {
+                                        Movement = 0,
+                                        WeaponSkill = 0,
+                                        BallisticSkill = 0,
+                                        Strength = 0,
+                                        Toughness = 7,
+                                        Wounds = 3,
+                                        Initiative = 0,
+                                        Attacks = 0,
+                                        Moral = 0
+                                    }
+                                },
+                                new SingleModel()
+                                {
+                                    Name = "Streitwagenlenker",
+                                    Profile = new Profile
+                                    {
+                                        Movement = 5,
+                                        WeaponSkill = 5,
+                                        BallisticSkill = 4,
+                                        Strength = 4,
+                                        Toughness = 3,
+                                        Wounds = 1,
+                                        Initiative = 7,
+                                        Attacks = 1,
+                                        Moral = 8
+                                    }
+                                },
+                                new SingleModel()
+                                {
+                                    Name = "Elfenroﬂ",
+                                    Profile = new Profile
+                                    {
+                                        Movement = 9,
+                                        WeaponSkill = 3,
+                                        BallisticSkill = 0,
+                                        Strength = 3,
+                                        Toughness = 3,
+                                        Wounds = 1,
+                                        Initiative = 4,
+                                        Attacks = 1,
+                                        Moral = 5
+                                    }
+                                }
+                            }
+                        })
+                    }
+                }
+            };
             RootItems = new ObservableCollection<ArmyTreeNode>
             {
                 new ArmyTreeNode(army)
-                {
-                    Children = new ObservableCollection<UnitTreeNode>
-                    {
-                        new UnitTreeNode(
-                            new Unit("Generalseinheit")
-                            {
-                                MainModels = new List<Pair<int, MainModel>>
-                                {
-                                    new Pair<int, MainModel>(1, new MainModel()
-                                    {
-                                        Name = "General",
-                                        Points = 100,
-                                        SingleModels = new List<SingleModel>
-                                        {
-                                            new SingleModel()
-                                            {
-                                                Name = "General",
-                                                Profile = new Profile
-                                                {
-                                                    Movement = 5,
-                                                    WeaponSkill = 7,
-                                                    BallisticSkill = 7,
-                                                    Strength = 4,
-                                                    Toughness = 4,
-                                                    Wounds = 3,
-                                                    Initiative = 9,
-                                                    Attacks = 4,
-                                                    Moral = 10
-                                                }
-                                            }
-                                        }
-                                    }),
-                                    new Pair<int, MainModel>(20, new MainModel()
-                                    {
-                                        Name = "Speertr‰ger",
-                                        Points = 10,
-                                        SingleModels = new List<SingleModel>
-                                        {
-                                            new SingleModel()
-                                            {
-                                                Name = "Speertr‰ger",
-                                                Profile = new Profile
-                                                {
-                                                    Movement = 5,
-                                                    WeaponSkill = 4,
-                                                    BallisticSkill = 4,
-                                                    Strength = 3,
-                                                    Toughness = 3,
-                                                    Wounds = 1,
-                                                    Initiative = 6,
-                                                    Attacks = 1,
-                                                    Moral = 8
-                                                }
-                                            }
-                                        }
-                                    })
-                                }
-
-                            }
-                        ),
-                        new UnitTreeNode(
-                            new Unit("Streitwagen")
-                            {
-                                MainModels = new List<Pair<int, MainModel>>
-                                {
-                                    new Pair<int, MainModel>(1, new MainModel()
-                                    {
-                                        Name = "Streitwagen",
-                                        Points = 70,
-                                        SingleModels = new List<SingleModel>
-                                        {
-                                            new SingleModel()
-                                            {
-                                                Name = "Streitwagen",
-                                                Profile = new Profile
-                                                {
-                                                    Movement = 0,
-                                                    WeaponSkill = 0,
-                                                    BallisticSkill = 0,
-                                                    Strength = 0,
-                                                    Toughness = 7,
-                                                    Wounds = 3,
-                                                    Initiative = 0,
-                                                    Attacks = 0,
-                                                    Moral = 0
-                                                }
-                                            },
-                                            new SingleModel()
-                                            {
-                                                Name = "Streitwagenlenker",
-                                                Profile = new Profile
-                                                {
-                                                    Movement = 5,
-                                                    WeaponSkill = 5,
-                                                    BallisticSkill = 4,
-                                                    Strength = 4,
-                                                    Toughness = 3,
-                                                    Wounds = 1,
-                                                    Initiative = 7,
-                                                    Attacks = 1,
-                                                    Moral = 8
-                                                }
-                                            },
-                                            new SingleModel()
-                                            {
-                                                Name = "Elfenroﬂ",
-                                                Profile = new Profile
-                                                {
-                                                    Movement = 9,
-                                                    WeaponSkill = 3,
-                                                    BallisticSkill = 0,
-                                                    Strength = 3,
-                                                    Toughness = 3,
-                                                    Wounds = 1,
-                                                    Initiative = 4,
-                                                    Attacks = 1,
-                                                    Moral = 5
-                                                }
-                                            }
-                                        }
-                                    })
-                                }
-
-                            }
-                        )
-                    }
-                }
             };
         }
     }
