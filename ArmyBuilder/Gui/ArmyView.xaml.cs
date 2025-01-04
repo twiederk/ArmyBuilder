d43fe4384d5d00f0bd8d061a9df110bc0720f47e
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-01-04 06:39:41 +0100

    Removed code duplicated to ArmyListControl

--- ArmyBuilder/Gui/ArmyView.xaml.cs
+++ ArmyBuilder/Gui/ArmyView.xaml.cs
@@ -43,44 +43,6 @@ namespace ArmyBuilder
             window.Content = _serviceProvider.GetRequiredService<StartView>();
         }
 
-        private void ListBox_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                ListBox listBox = sender as ListBox;
-                if (listBox != null)
-                {
-                    var selectedItem = listBox.SelectedItem;
-
-                    if (selectedItem != null)
-                    {
-                        DataObject dataObject = new DataObject(selectedItem);
-                        DragDrop.DoDragDrop(listBox, dataObject, DragDropEffects.Move);
-                    }
-                }
-            }
-        }
-
-        private void ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            ListBox listBox = sender as ListBox;
-            if (listBox != null)
-            {
-                var selectedMainModel = listBox.SelectedItem as MainModel;
-                if (selectedMainModel != null)
-                {
-                    MainModel clonedMainModel = selectedMainModel.Clone();
-                    ArmyViewModel armyViewModel = DataContext as ArmyViewModel;
-                    ArmyTreeViewModel armyTreeViewModel = armyViewModel.ArmyTreeViewModel;
-                    Army army = armyTreeViewModel.Army;
-                    Unit unit = armyViewModel.CreateUnit(army, clonedMainModel);
-                    ArmyTreeNode armyTreeNode = armyTreeViewModel.Root[0];
-                    armyTreeNode.AddUnit(unit);
-                }
-            }
-        }
-
-
         private void armyTreeNode_Drop(object sender, DragEventArgs dragEvent)
         {
             if (dragEvent.Data.GetDataPresent(typeof(MainModel)))
