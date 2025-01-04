248b22aca3b1fc351c821f91a2a1afda6b39b6f2
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-01-04 07:18:24 +0100

    Added heading of profile

--- ArmyBuilder/Print/MainModelPrintComponent.cs
+++ ArmyBuilder/Print/MainModelPrintComponent.cs
@@ -1,6 +1,8 @@
 ﻿using ArmyBuilder.Domain;
+using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 using QuestPDF.Fluent;
+//using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 
 namespace ArmyBuilder.Print
 {
@@ -15,14 +17,69 @@ namespace ArmyBuilder.Print
 
         public void Compose(IContainer container)
         {
-            container.PaddingLeft(10).Column(column =>
+
+            container.PaddingLeft(20).Table(table =>
             {
-                column.Spacing(2);
-                column.Item().Text(Heading());
-                foreach (var singleModel in _mainModel.SingleModels)
+                table.ColumnsDefinition(columns =>
                 {
-                    column.Item().Component(new SingleModelPrintComponent(singleModel));
+                    columns.ConstantColumn(150);
+                    columns.RelativeColumn(20);
+                    columns.RelativeColumn(20);
+                    columns.RelativeColumn(20);
+                    columns.RelativeColumn(20);
+                    columns.RelativeColumn(20);
+                    columns.RelativeColumn(20);
+                    columns.RelativeColumn(20);
+                    columns.RelativeColumn(20);
+                    columns.RelativeColumn(20);
+                });
+
+                table.Header(header =>
+                {
+                    header.Cell().Element(CellStyle).Text("Name");
+                    header.Cell().Element(CellStyle).Text("B");
+                    header.Cell().Element(CellStyle).Text("WS");
+                    header.Cell().Element(CellStyle).Text("BS");
+                    header.Cell().Element(CellStyle).Text("S");
+                    header.Cell().Element(CellStyle).Text("W");
+                    header.Cell().Element(CellStyle).Text("LP");
+                    header.Cell().Element(CellStyle).Text("I");
+                    header.Cell().Element(CellStyle).Text("A");
+                    header.Cell().Element(CellStyle).Text("MW");
+
+                    static IContainer CellStyle(IContainer container)
+                    {
+                        return container.DefaultTextStyle(x => x.SemiBold()).PaddingVertical(5).BorderBottom(1).BorderColor(Colors.Black);
+                    }
+                });
+
+                foreach (SingleModel singleModel in _mainModel.SingleModels)
+                {
+                    table.Cell().Element(CellStyle).Text(singleModel.Name);
+                    table.Cell().Element(CellStyle).Text(singleModel.Profile.Movement);
+                    table.Cell().Element(CellStyle).Text(singleModel.Profile.WeaponSkill);
+                    table.Cell().Element(CellStyle).Text(singleModel.Profile.BallisticSkill);
+                    table.Cell().Element(CellStyle).Text(singleModel.Profile.Strength);
+                    table.Cell().Element(CellStyle).Text(singleModel.Profile.Toughness);
+                    table.Cell().Element(CellStyle).Text(singleModel.Profile.Wounds);
+                    table.Cell().Element(CellStyle).Text(singleModel.Profile.Initiative);
+                    table.Cell().Element(CellStyle).Text(singleModel.Profile.Attacks);
+                    table.Cell().Element(CellStyle).Text(singleModel.Profile.Moral);
+
+                    static IContainer CellStyle(IContainer container)
+                    {
+                        return container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
+                    }
                 }
+
+                //    container.PaddingLeft(10).Column(column =>
+                //{
+                //    column.Spacing(2);
+                //    column.Item().Text(Heading());
+                //    foreach (var singleModel in _mainModel.SingleModels)
+                //    {
+                //        column.Item().Component(new SingleModelPrintComponent(singleModel));
+                //    }
             });
         }
 
