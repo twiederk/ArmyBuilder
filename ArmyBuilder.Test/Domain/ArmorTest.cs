a8f39f117cb6b292447058d25051f062ad0f5658
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-01-05 06:34:29 +0100

    Created classes Item and Armor

---
+++ ArmyBuilder.Test/Domain/ArmorTest.cs
@@ -0,0 +1,21 @@
+﻿using ArmyBuilder.Domain;
+using FluentAssertions;
+
+namespace ArmyBuilder.Test.Domain
+{
+    public class ArmorTest
+    {
+
+        [Fact]
+        public void should_create_armor_with_name()
+        {
+            // act
+            Armor armor = new Armor() { Id = 1, Name = "Test Armor" };
+
+            // assert
+            armor.Id.Should().Be(1);
+            armor.Name.Should().Be("Test Army");
+        }
+
+    }
+}
---
+++ ArmyBuilder/Domain/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace ArmyBuilder.Domain
+{
+    public class Item
+    {
+
+        public int Id;
+        public string Name;
+        public string Description;
+        public ArmyList ArmyList;
+        public bool Magic;
+        public bool Points;
+    }
+}
