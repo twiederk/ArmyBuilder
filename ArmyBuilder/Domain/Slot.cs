4974a6fab4babf4fd2f96b36c15bba1a3398fc53
IN57KC <torsten.wiederkehr@ing.com>
2025-01-08 15:38:55 +0100

    Removed unnecessary methods

--- ArmyBuilder/Domain/Slot.cs
+++ ArmyBuilder/Domain/Slot.cs
@@ -4,13 +4,10 @@ namespace ArmyBuilder.Domain
 {
     public class Slot
     {
-        public string ItemName => $"{Item.Name} ({Item.Points})";
         public int Id { get; set; }
         public Item Item { get; set; }
         public bool Editable { get; set; }
         public bool AllItems { get; set; }
         public List<Item> SelectableItems { get; set; }
-
-        public string Name => "SlotName";
     }
 }
