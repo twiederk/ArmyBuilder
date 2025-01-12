87321f25eff3c825b38bafd771b9354ddc8f8ef6
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-01-12 08:23:44 +0100

    Start implementation of assignement of equipment

--- ArmyBuilder/Domain/Equipment.cs
+++ ArmyBuilder/Domain/Equipment.cs
@@ -1,10 +1,25 @@
 namespace ArmyBuilder.Domain
 {
-
     public class Equipment
     {
         public int Id { get; set; }
         public List<Slot> Slots { get; set; } = new List<Slot>();
-    }
 
+        public override bool Equals(object obj)
+        {
+            if (obj is Equipment other)
+            {
+                return Id == other.Id;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
 }
+
+
+
