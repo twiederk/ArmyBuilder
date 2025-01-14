35b66d28b9f6bf55e2e8a348e24ccf3a901e281e
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-01-14 05:59:20 +0100

    Added test for SlotViewModel.SlotName()

---
+++ ArmyBuilder.Test/ViewModels/SlotViewModelTest.cs
@@ -0,0 +1,23 @@
+﻿using ArmyBuilder.Domain;
+using ArmyBuilder.ViewModels;
+using FluentAssertions;
+
+namespace ArmyBuilder.Test.ViewModels
+{
+    public class SlotViewModelTest
+    {
+        [Fact]
+        public void should_return_correct_slot_name_for_melee_weapon()
+        {
+            // arrange
+            Slot slot = new Slot() { ItemClass = ItemClass.MeleeWeapon };
+            SlotViewModel slotViewModel = new SlotViewModel(slot);
+
+            // act
+            string slotName = slotViewModel.SlotName();
+
+            // assert
+            slotName.Should().Be("Waffe");
+        }
+    }
+}
