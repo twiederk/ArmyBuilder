bbc673c2e80ee0908190614a20ec9c642047d56f
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-01-24 15:37:32 +0100

    Removed debugging code

--- ArmyBuilder.Test/ViewModels/ArmyViewModelIntegrationTest.cs
+++ ArmyBuilder.Test/ViewModels/ArmyViewModelIntegrationTest.cs
@@ -76,13 +76,6 @@ namespace ArmyBuilder.Test.ViewModels
             var result = _armyViewModel.selectableItems(slot, highElfArmyList);
 
             // assert
-            var expectedIds = new List<int>
-            {
-                1, 2, 3, 4, 5, 6, 5638, 5639, 5640, 5641, 5643, 5644, 5645, 5649, 5650, 5652, 5653, 5657, 5658, 5660, 5661, 5662, 5663, 5666, 5667, 5669, 5670, 5671, 5672, 5673, 5674, 5675, 5676, 5677, 5678, 5679, 5680, 5681, 5682, 5683, 5684, 5685, 5686, 5687, 5688, 5689, 5691, 5694, 5695, 5696, 5703, 5708, 5712, 5717, 5733, 5738, 5869, 5899, 5904, 5925, 5932, 5940, 5989, 6056
-            };
-            var resultIds = result.Select(i => i.Id).ToList();
-            var missingIds = expectedIds.Except(resultIds).ToList();
-            // missingIds = { 5717, 5733, 5869, 5899, 5904, 5925, 5932, 5940, 5989, 6056 }
             result.Should().HaveCount(64);
         }
 
