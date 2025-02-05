6806bd8a5cdde6a18464442bb51de2a075f86437
IN57KC <torsten.wiederkehr@ing.com>
2025-02-05 15:34:38 +0100

    Added light and heavy armor with harnish

--- ArmyBuilder/db/data.sql
+++ ArmyBuilder/db/data.sql
@@ -3473,6 +3473,8 @@ INSERT INTO armor VALUES(42,'Schwere Rüstung',3,'',-1,0,0,2);
 INSERT INTO armor VALUES(43,'Chaosrüstung',10,'',1,0,0,3);
 INSERT INTO armor VALUES(44,'Drachenumhang',5,'',5,0,0,2);
 INSERT INTO armor VALUES(45,'Scale Skin',0,'',6,0,0,2);
+INSERT INTO armor VALUES(46,'Leichte Rüstung und Roßharnisch',6,'',-1,0,0,2);
+INSERT INTO armor VALUES(47,'Schwere Rüstung und Roßharnisch',7,'',-1,0,0,3);
 INSERT INTO armor VALUES(51,'Roßharnisch',4,'',-1,0,0,1);
 INSERT INTO armor VALUES(5781,'Chaosrüstung',10,'Rettungswurf 4+.',1,0,1,0);
 INSERT INTO armor VALUES(5782,'Meteorrüstung',25,'Rettungswurf 2+.',-1,0,1,0);
