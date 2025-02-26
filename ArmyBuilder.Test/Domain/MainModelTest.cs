a2e9db3fcd9f3b80413c8add1405465dc62aaea8
IN57KC <torsten.wiederkehr@ing.com>
2025-02-26 13:39:04 +0100

    Implemented trooper on mount with musician

--- ArmyBuilder.Test/Domain/MainModelTest.cs
+++ ArmyBuilder.Test/Domain/MainModelTest.cs
@@ -493,5 +493,45 @@ namespace ArmyBuilder.Test.Domain
             points.Should().Be(380);
         }
 
+        [Fact]
+        public void should_calculate_points_of_trooper_on_mount_with_musician()
+        {
+            // arrange
+            var mainModel = new MainModel
+            { 
+                ArmyCategory = ArmyCategory.Trooper,
+                Count = 10,
+                Musician = true,
+                SingleModels =
+                { 
+                    new SingleModel
+                    {
+                        Name = "Knight",
+                        Profile = new Profile { Points = 10 },
+                        MovementType = MovementType.OnMount,
+                        Equipment = new Equipment
+                        {
+                            Slots =
+                            {
+                                new Slot { Item = new MeleeWeapon { Name = "Lance", Points = 2 } },
+                                new Slot { Item = new Armor { Name = "heavy Armor", Points = 3 } },
+                                new Slot { Item = new Shield { Name = "Shield", Points = 1 } },
+                            }
+                        }
+                    },
+                    new SingleModel {
+                        Name = "Horse",
+                        Profile = new Profile { Points = 3}
+                    }
+                }
+            };
+            
+            // act
+            float points = mainModel.TotalPoints();
+
+            // assert
+            points.Should().Be(456);
+        }
+
     }
 }
