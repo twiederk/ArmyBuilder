3a0273c1b44fbd37fcbb29be81a1da41ac530d33
IN57KC <torsten.wiederkehr@ing.com>
2025-03-05 13:22:14 +0100

    WiP: Start implementation of melee weapon strength

--- ArmyBuilder/Domain/MeleeWeapon.cs
+++ ArmyBuilder/Domain/MeleeWeapon.cs
@@ -1,9 +1,31 @@
-﻿using Dapper.Contrib.Extensions;
+﻿using System.Data.SQLite;
+using System.Reflection;
+using Dapper.Contrib.Extensions;
 
 namespace ArmyBuilder.Domain
 {
     [Table("melee_weapon")]
     public class MeleeWeapon: Item
     {
+        public int Strength { get; set; }
+
+        const int ID_TWO_HANDED_WEAPON = 1000;
+        const int ID_LANCE = 1001;
+
+        public string DisplayStrength(int profileStrength)
+        {
+            int totalStrength = profileStrength + Strength;
+
+            switch (Id)
+            {
+                case ID_TWO_HANDED_WEAPON:
+                    return $"*{totalStrength}*";
+                case ID_LANCE:
+                    return $"{totalStrength}/{profileStrength}";
+                default:
+                    return totalStrength.ToString();
+            }
+        }
     }
+
 }
