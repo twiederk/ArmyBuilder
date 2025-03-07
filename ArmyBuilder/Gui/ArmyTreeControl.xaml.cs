e8b11e9287eed8d683eb49f419ce52c65f311867
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-03-07 06:55:05 +0100

    Added buttons to expand and collapse army tree

--- ArmyBuilder/Gui/ArmyTreeControl.xaml.cs
+++ ArmyBuilder/Gui/ArmyTreeControl.xaml.cs
@@ -13,6 +13,73 @@ namespace ArmyBuilder
             InitializeComponent();
         }
 
+        private void CollapseLevel1_Click(object sender, RoutedEventArgs e)
+        {
+            CollapseTreeViewItems(trvArmy, 1);
+        }
+
+        private void CollapseLevel2_Click(object sender, RoutedEventArgs e)
+        {
+            CollapseTreeViewItems(trvArmy, 2);
+        }
+
+        private void CollapseLevel3_Click(object sender, RoutedEventArgs e)
+        {
+            CollapseTreeViewItems(trvArmy, 3);
+        }
+
+        private void CollapseTreeViewItems(ItemsControl parent, int level)
+        {
+            foreach (var item in parent.Items)
+            {
+                if (parent.ItemContainerGenerator.ContainerFromItem(item) is TreeViewItem treeViewItem)
+                {
+                    if (level == 1)
+                    {
+                        treeViewItem.IsExpanded = false;
+                    }
+                    else
+                    {
+                        CollapseTreeViewItems(treeViewItem, level - 1);
+                    }
+                }
+            }
+        }
+
+        private void ExpandLevel1_Click(object sender, RoutedEventArgs e)
+        {
+            ExpandTreeViewItems(trvArmy, 1);
+        }
+
+        private void ExpandLevel2_Click(object sender, RoutedEventArgs e)
+        {
+            ExpandTreeViewItems(trvArmy, 2);
+        }
+
+        private void ExpandLevel3_Click(object sender, RoutedEventArgs e)
+        {
+            ExpandTreeViewItems(trvArmy, 3);
+        }
+
+        private void ExpandTreeViewItems(ItemsControl parent, int level)
+        {
+            foreach (var item in parent.Items)
+            {
+                if (parent.ItemContainerGenerator.ContainerFromItem(item) is TreeViewItem treeViewItem)
+                {
+                    if (level == 1)
+                    {
+                        treeViewItem.IsExpanded = true;
+                    }
+                    else
+                    {
+                        treeViewItem.IsExpanded = true;
+                        ExpandTreeViewItems(treeViewItem, level - 1);
+                    }
+                }
+            }
+        }
+
         private void armyTreeNode_Drop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(typeof(MainModel)))
