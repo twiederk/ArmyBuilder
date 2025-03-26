a65cc861e4bad700531f6dcc26395730a360b709
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-03-26 06:15:41 +0100

    Fixed display in MainModelControl

--- ArmyBuilder/ViewModels/ArmyMainModelViewModel.cs
+++ ArmyBuilder/ViewModels/ArmyMainModelViewModel.cs
@@ -10,6 +10,9 @@ namespace ArmyBuilder.ViewModels
         public float NewPoints => MainModel.NewPoints;
         public float OldPoints => MainModel.OldPoints;
         public string NumberOfFigures => MainModel.NumberOfFigures.ToString();
+        public List<SingleModel> SingleModels => MainModel.SingleModels;
+        public string Description => MainModel.Description;
+
         public MainModel MainModel { get; set; }
 
         public ArmyMainModelViewModel(MainModel mainModel)
