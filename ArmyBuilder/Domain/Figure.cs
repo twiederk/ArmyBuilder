c2752ee9a49301af3d2ebee0517be7ed83774cf5
IN57KC <torsten.wiederkehr@ing.com>
2025-03-26 13:16:50 +0100

    Added Figure class

---
+++ ArmyBuilder/Domain/Figure.cs
@@ -0,0 +1,14 @@
+using System;
+using Dapper.Contrib.Extensions;
+
+
+namespace ArmyBuilder.Domain
+{
+    [Table("figure")]
+    public class Figure
+    {
+        public int Id { get; set; }
+        public int NumberOfFigures { get; set;} = new Random().Next(0, 21);
+        public string ImagePath { get; set; } = new Random().Next(0, 1) == 0 ? @"images\Dwarves\Dwarf_JosefBugman.jpg" : @"images\Dwarves\Dwarf_Runenamboss.jpg"; 
+    }
+}
