3f3b8f116b385124e6b90633f67926f6ee798838
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-03-28 06:11:33 +0100

    Removed image path form dummy image path

--- ArmyBuilder/Domain/Figure.cs
+++ ArmyBuilder/Domain/Figure.cs
@@ -9,6 +9,6 @@ namespace ArmyBuilder.Domain
     {
         public int Id { get; set; }
         public int NumberOfFigures { get; set;} = new Random().Next(0, 21);
-        public string ImagePath { get; set; } = new Random().Next(0, 2) == 0 ? @"images\Dwarves\Dwarf_JosefBugman.jpg" : @"images\Dwarves\Dwarf_Runenamboss.jpg"; 
+        public string ImagePath { get; set; } = new Random().Next(0, 2) == 0 ? @"Dwarves\Dwarf_JosefBugman.jpg" : @"Dwarves\Dwarf_Runenamboss.jpg"; 
     }
 }
