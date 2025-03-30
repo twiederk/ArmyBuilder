62614d343ead6ae3e2efb2fbfaf27bec382e62d4
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-03-30 07:03:46 +0200

    Removed MainModel.ImagePath and MainModel.NumberOfFigures

--- ArmyBuilder/Dao/ArmyListRepositorySqlite.cs
+++ ArmyBuilder/Dao/ArmyListRepositorySqlite.cs
@@ -23,7 +23,7 @@ namespace ArmyBuilder.Dao
         {
             var sql = @"
                 SELECT 
-                    mm.id, mm.army_category_id as ArmyCategory, mm.name, mm.description, mm.points as OldPoints, mm.Uniquely, mm.standard_bearer AS StandardBearer, mm.musician, mm.image_path AS ImagePath, mm.number_of_figures AS NumberOfFigures,
+                    mm.id, mm.army_category_id as ArmyCategory, mm.name, mm.description, mm.points as OldPoints, mm.Uniquely, mm.standard_bearer AS StandardBearer, mm.musician,
                     sm.Id, sm.Name, sm.profile_id as ProfileId, sm.movement_type_id as MovementType, sm.mount, sm.mountable, sm.count,
                     p.Id, p.Movement, p.weapon_skill as WeaponSkill, p.ballistic_skill as BallisticSkill, p.Strength, p.Toughness, p.Wounds, p.Initiative, p.Attacks, p.Moral, p.Points, p.Save,
                     f.Id, f.number_of_figures as NumberOfFigures, f.image_path as ImagePath
@@ -134,7 +134,7 @@ namespace ArmyBuilder.Dao
         {
             var sql = @"
                 SELECT 
-                    mm.Id, mm.army_category_id as ArmyCategory, mm.Name, mm.Description, mm.Points, mm.Uniquely, mm.standard_bearer AS StandardBearer, mm.musician, mm.image_path AS ImagePath, mm.number_of_figures AS NumberOfFigures,
+                    mm.Id, mm.army_category_id as ArmyCategory, mm.Name, mm.Description, mm.Points, mm.Uniquely, mm.standard_bearer AS StandardBearer, mm.musician,
                     sm.Id, sm.Name, sm.profile_id as ProfileId, sm.movement_type_id as MovementType, sm.mount, sm.mountable, sm.count,
                     p.Id, p.Movement, p.weapon_skill as WeaponSkill, p.ballistic_skill as BallisticSkill, p.Strength, p.Toughness, p.Wounds, p.Initiative, p.Attacks, p.Moral, p.Points, p.Save,
                     f.Id, f.number_of_figures as NumberOfFigures, f.image_path as ImagePath
