b1326bfaf07cc7cbb49ab95c92367b38ac86e086
IN57KC <torsten.wiederkehr@ing.com>
2025-04-08 11:29:07 +0200

    Display profile of mount in SelectMountWindow

--- ArmyBuilder/ViewModels/MountViewModel.cs
+++ ArmyBuilder/ViewModels/MountViewModel.cs
@@ -6,6 +6,17 @@ namespace ArmyBuilder.ViewModels
     public class MountViewModel
     {
         public string Name => SingleModel.DisplayName;
+        public string Movement => SingleModel.Movement;
+        public string WeaponSkill => SingleModel.WeaponSkill;
+        public string BallisticSkill => SingleModel.BallisticSkill;
+        public string Strength => SingleModel.Strength;
+        public string Toughness => SingleModel.Toughness;
+        public string Wounds => SingleModel.Wounds;
+        public string Initiative => SingleModel.Initiative;
+        public string Attacks => SingleModel.Attacks;
+        public string Moral => SingleModel.Moral;
+        public string Save => SingleModel.Save;
+
         public SingleModel SingleModel { get; set;}
 
         public MountViewModel(SingleModel singleModel)
