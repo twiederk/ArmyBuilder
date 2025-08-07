5c19ae7fe4a11d99418e2b69ad5a2c9202630ebf
IN57KC <torsten.wiederkehr@ing.com>
2025-08-07 15:56:47 +0200

    Fixed weapon skill of Ork Basis

--- ArmyBuilder/db/0002_data.sql
+++ ArmyBuilder/db/0002_data.sql
@@ -395,7 +395,7 @@ INSERT INTO profile VALUES(11844,'Dunkelelfen Schwarzer Streitwagen',NULL,NULL,N
 INSERT INTO profile VALUES(11846,'',4,3,4,3,3,1,3,1,7,0.0,7);
 INSERT INTO profile VALUES(11849,'',7,3,0,4,4,2,2,1,3,0.0,7);
 INSERT INTO profile VALUES(11850,'Orks Wildork Basis',4,3,3,3,4,1,2,2,7,6.5,7);
-INSERT INTO profile VALUES(11851,'Orks Ork Basis',4,4,3,3,4,1,2,1,7,5.5,7);
+INSERT INTO profile VALUES(11851,'Orks Ork Basis',4,3,3,3,4,1,2,1,7,5.5,7);
 INSERT INTO profile VALUES(11852,'Orks Goblin Basis',4,2,3,3,3,1,2,1,5,2.5,7);
 INSERT INTO profile VALUES(11853,'Orks Waldgoblin Basis',4,2,3,3,3,1,2,1,5,2.5,7);
 INSERT INTO profile VALUES(11854,'Orks Grobgitz Basis',4,4,3,4,4,1,3,1,7,6.5,7);
